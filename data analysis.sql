a14de353305bd4b0470bbdd6b85e89ebf652aad9
jeaniewwu <95399587+jeaniewwu@users.noreply.github.com>
2022-04-14 20:14:35 -0700

    Update data analysis.sql

--- data analysis.sql
+++ data analysis.sql
@@ -15,7 +15,7 @@ from employees
 where hire_date between '1986-01-01' AND '1986-12-31'
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-select departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name, dept_manager.from_date, dept_manager.to_date
+select departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
 from departments
 join dept_manager on departments.dept_no = dept_manager.dept_no
 join employees on dept_manager.emp_no = employees.emp_no;
